997dc5e350c3a20adbd29426b34dd5f485f480c1
longwaye <73245683+longwaye@users.noreply.github.com>
2021-07-22 18:01:37 +0800

    longwaye

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:longwaye/longwaye.github.io.git main
+# git push -f git@github.com:longwaye/longwaye.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:longwaye/notes.git master:gh-pages
 
 cd -
